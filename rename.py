dfb02c969e7038511cea6a2b3cdf57fc99aba4ef
Hur Suhkjin <sjin.75@gmail.com>
2018-10-29 22:36:29 +0900

    changes for python3

--- rename.py
+++ rename.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 import os
 
-for i in range(51,66,1):
+for i in range(1,2,1):
     os.rename('kc-v9-n2-' + str(i) + '.indd', 'kc-v9-n2-' + str(i-1) + '.indd')
-    print 'kc-v9-n2-' + str(i) + '.indd' + ' to ' + 'kc-v9-n2-' + str(i-1) + '.indd'
-
+    print('kc-v9-n2-' + str(i) + '.indd' + ' to ' + 'kc-v9-n2-' + str(i-1) + '.indd')
